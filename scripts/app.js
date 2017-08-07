11bc70b48806121aec24afa648c39b2c42a214db
logic0verflow <git@nathanacosta.com>
2017-08-07 13:34:43 -0600

    Update How The Story Element Styles are Changed  - Update scripts/app.js so that only story elements within the viewport are updated and ignore the rest. Reduce the complexity of the math used to update styles.

--- scripts/app.js
+++ scripts/app.js
@@ -258,30 +258,41 @@ APP.Main = (function() {
 
     // It does seem awfully broad to change all the
     // colors every time!
-    for (var s = 0; s < storyElements.length; s++) {
+
+    var storyElementAmt = storyElements.length;
+
+    for (var s = 0; s < storyElementAmt; s++) {
+
+      var height = main.offsetHeight;
 
       var story = storyElements[s];
-      var score = story.querySelector('.story__score');
-      var title = story.querySelector('.story__title');
+      var storyTop = story.getBoundingClientRect().top;
+      // var storyBottom = story.getBoundingClientRect().bottom;
 
-      // Base the scale on the y position of the score.
-      var height = main.offsetHeight;
-      var mainPosition = main.getBoundingClientRect();
-      var scoreLocation = score.getBoundingClientRect().top -
-          document.body.getBoundingClientRect().top;
-      var scale = Math.min(1, 1 - (0.05 * ((scoreLocation - 170) / height)));
-      var opacity = Math.min(1, 1 - (0.5 * ((scoreLocation - 170) / height)));
+      if (storyTop >= 0 && storyTop < height) {
+        var score = story.querySelector('.story__score');
+        var title = story.querySelector('.story__title');
+        // var mainPosition = main.getBoundingClientRect();
+        var location = height - storyTop;
+        var saturation = 100 * ((location + 170) / height);
+        score.style.backgroundColor = 'hsl(42, ' + saturation + '%, 50%)';
 
-      score.style.width = (scale * 40) + 'px';
-      score.style.height = (scale * 40) + 'px';
-      score.style.lineHeight = (scale * 40) + 'px';
+        var opacity = Math.min(1, 1 - (0.5 * ((storyTop - 170) / height)));
+        title.style.opacity = opacity;
+
+      }
+
+
+
+
+      // score.style.width = (scale * 40) + 'px';
+      // score.style.height = (scale * 40) + 'px';
+      // score.style.lineHeight = (scale * 40) + 'px';
 
       // Now figure out how wide it is and use that to saturate it.
-      scoreLocation = score.getBoundingClientRect();
-      var saturation = (100 * ((scoreLocation.width - 38) / 2));
+      // scoreLocation = score.getBoundingClientRect();
+      // var saturation = (100 * ((scoreLocation.width - 38) / 2));
 
-      score.style.backgroundColor = 'hsl(42, ' + saturation + '%, 50%)';
-      title.style.opacity = opacity;
     }
   }
 
